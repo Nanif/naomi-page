d46d16b14bdf8cb6cc9bdf9c1839fb0100fb5795
neomi2152@gmail.com <neomi2152@gmail.com>
2022-06-12 16:38:19 +0300

    bootstrap

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import { store } from './Smarket/Store';
 import {router}from './router'
 import BootstrapVue from "bootstrap-vue"
-// import VeeValidate from 'vee-validate';
 
 import App from './App'
 
@@ -12,7 +11,6 @@ import Pages from './Layout/Wrappers/pagesLayout.vue';
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
-// Vue.use(VeeValidate);
 
 Vue.component('default-layout', Default);
 Vue.component('userpages-layout', Pages);
